af4a4fd076db7a5c2943afda76cf4767457a8bfc
dpaschal <daniel.paschal@gmail.com>
2015-03-12 22:21:40 -0700

    more comments

--- upload.php
+++ upload.php
@@ -14,18 +14,19 @@
         //if the save_button was clicked, it's value will be 'save'.
         //save the value from the 'animal_input' field into our file
         if($_POST['save_button']=='save'){
-            file_put_contents('animalfile.txt',$_POST['animal_input']."\n",FILE_APPEND);
+            
+            file_put_contents('animalfile.txt',$_POST['animal_input']."\n",FILE_APPEND);//put the contents of $_POST['animal_input'] into the file 'animalfile.txt'.  FILE_APPEND causes the next contents to be added to the end of the file, rather than replacing it
             
         }
     ?>
     
     <?php
-        //get the contents of our file and spit them out in order
-        $file_contents = file_get_contents('animalfile.txt');
-        $data_array = explode("\n",$file_contents);
-        foreach($data_array as $value)
+        
+        $file_contents = file_get_contents('animalfile.txt');//get the contents of our file 
+        $data_array = explode("\n",$file_contents); //break up the string into an array elemeny every time a newline character (\n) is encountered
+        foreach($data_array as $value)  //loop through each part of the array
         {
-            print("<div>$value</div>");
+            print("<div>$value</div>");  //and print a new div for each item
         }
     ?>
     
